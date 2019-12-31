7c7be0356981f46cc10f542719eb993187fdbedb
Rishabh027 <39622682+Rishabh027@users.noreply.github.com>
2019-12-31 20:48:21 +0530

    name_changed
    Little bit more changes

---
+++ Bulk-compression.py
@@ -0,0 +1,13 @@
+import subprocess as sb
+import os
+path=input("Enter Path to perform compression(Use format E:\Seasons\Silicon.Valley.2014): ")##Enter path in this format E:\Seasons\Silicon.Valley.2014
+ls=os.listdir(path)
+for i in ls:
+    s=path+"\\"+i
+    d=s+".zip"
+    cmd="powershell Compress-Archive "+s+" "+d
+    sb.call(cmd,shell=True)
+
+print("Compression Done!!")
+    
+
